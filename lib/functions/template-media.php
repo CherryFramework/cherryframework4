4bfb1d39781ee8e5dffac26146680fc315af960a
cheh <chehovskiy.dima@gmail.com>
2014-08-21 11:14:30 +0300

    Make sure to escape unknown variables in the media meta functions

--- lib/functions/template-media.php
+++ lib/functions/template-media.php
@@ -63,31 +63,31 @@ function cherry_audio_meta( $post_id, $metadata ) {
 
 	// Formated length of time the audio file runs.
 	if ( !empty( $metadata['length_formatted'] ) )
-		$items['length_formatted'] = array( $metadata['length_formatted'], $id3_keys['length_formatted'] );
+		$items['length_formatted'] = array( esc_html( $metadata['length_formatted'] ), $id3_keys['length_formatted'] );
 
 	// Artist.
 	if ( !empty( $metadata['artist'] ) )
-		$items['artist'] = array( $metadata['artist'], $id3_keys['artist'] );
+		$items['artist'] = array( esc_html( $metadata['artist'] ), $id3_keys['artist'] );
 
 	// Album.
 	if ( !empty( $metadata['album'] ) )
-		$items['album'] = array( $metadata['album'], $id3_keys['album'] );
+		$items['album'] = array( esc_html( $metadata['album'] ), $id3_keys['album'] );
 
 	// Year.
 	if ( !empty( $metadata['year'] ) )
-		$items['year'] = array( $metadata['year'], $id3_keys['year'] );
+		$items['year'] = array( absint( $metadata['year'] ), $id3_keys['year'] );
 
 	// Genre.
 	if ( !empty( $metadata['genre'] ) )
-		$items['genre'] = array( $metadata['genre'], $id3_keys['genre'] );
+		$items['genre'] = array( esc_html( $metadata['genre'] ), $id3_keys['genre'] );
 
 	// File size.
 	if ( !empty( $metadata['filesize'] ) )
-		$items['filesize'] = array( size_format( $metadata['filesize'], 2 ), $id3_keys['filesize'] );
+		$items['filesize'] = array( size_format( strip_tags( $metadata['filesize'] ), 2 ), $id3_keys['filesize'] );
 
 	// Mime type.
 	if ( !empty( $metadata['mime_type'] ) )
-		$items['mime_type'] = array( $metadata['mime_type'], $id3_keys['mime_type'] );
+		$items['mime_type'] = array( esc_html( $metadata['mime_type'] ), $id3_keys['mime_type'] );
 
 	return apply_filters( 'cherry_audio_meta', $items );
 }
@@ -110,15 +110,15 @@ function cherry_video_meta( $post_id, $metadata ) {
 
 	// File size.
 	if ( !empty( $metadata['filesize'] ) )
-		$items['filesize'] = array( size_format( $metadata['filesize'], 2 ), $id3_keys['filesize'] );
+		$items['filesize'] = array( size_format( strip_tags( $metadata['filesize'] ), 2 ), $id3_keys['filesize'] );
 
 	// Mime type.
 	if ( !empty( $metadata['mime_type'] ) )
-		$items['mime_type'] = array( $metadata['mime_type'], $id3_keys['mime_type'] );
+		$items['mime_type'] = array( esc_html( $metadata['mime_type'] ), $id3_keys['mime_type'] );
 
 	// Formated length of time the video file runs.
 	if ( !empty( $metadata['length_formatted'] ) )
-		$items['length_formatted'] = array( $metadata['length_formatted'], $id3_keys['length_formatted'] );
+		$items['length_formatted'] = array( esc_html( $metadata['length_formatted'] ), $id3_keys['length_formatted'] );
 
 	// Dimensions (width x height in pixels).
 	if ( !empty( $metadata['width'] ) && !empty( $metadata['height'] ) )
@@ -152,25 +152,25 @@ function cherry_image_meta( $post_id, $metadata ) {
 
 	// If a timestamp exists, add it to the $items array.
 	if ( !empty( $metadata['image_meta']['created_timestamp'] ) )
-		$items['created_timestamp'] = array( date_i18n( get_option( 'date_format' ), $metadata['image_meta']['created_timestamp'] ), __( 'Date', 'cherry' ) );
+		$items['created_timestamp'] = array( date_i18n( get_option( 'date_format' ), strip_tags( $metadata['image_meta']['created_timestamp'] ) ), __( 'Date', 'cherry' ) );
 
 	// If a camera exists, add it to the $items array.
 	if ( !empty( $metadata['image_meta']['camera'] ) )
-		$items['camera'] = array( $metadata['image_meta']['camera'], __( 'Camera', 'cherry' ) );
+		$items['camera'] = array( esc_html( $metadata['image_meta']['camera'] ), __( 'Camera', 'cherry' ) );
 
 	// If an aperture exists, add it to the $items array.
 	if ( !empty( $metadata['image_meta']['aperture'] ) )
-		$items['aperture'] = array( sprintf( '<sup>f</sup>&frasl;<sub>%s</sub>', $metadata['image_meta']['aperture'] ), __( 'Aperture', 'cherry' ) );
+		$items['aperture'] = array( sprintf( '<sup>f</sup>&frasl;<sub>%s</sub>', absint( $metadata['image_meta']['aperture'] ) ), __( 'Aperture', 'cherry' ) );
 
 	// If a focal length is set, add it to the $items array.
 	if ( !empty( $metadata['image_meta']['focal_length'] ) )
 		// Translators: Camera focal length.
-		$items['focal_length'] = array( sprintf( __( '%s mm', 'cherry' ), $metadata['image_meta']['focal_length'] ), __( 'Focal Length', 'cherry' ) );
+		$items['focal_length'] = array( sprintf( __( '%s mm', 'cherry' ), absint( $metadata['image_meta']['focal_length'] ) ), __( 'Focal Length', 'cherry' ) );
 
 	// If an ISO is set, add it to the $items array.
 	if ( !empty( $metadata['image_meta']['iso'] ) ) {
 		$items['iso'] = array(
-			$metadata['image_meta']['iso'],
+			absint( $metadata['image_meta']['iso'] ),
 			'<abbr title="' . __( 'International Organization for Standardization', 'cherry' ) . '">' . __( 'ISO', 'cherry' ) . '</abbr>'
 		);
 	}
