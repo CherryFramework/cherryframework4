c5538d67c76ed1fda1a2854a2184fd37d24299db
cheh <chehovskiy.dima@gmail.com>
2014-11-14 17:18:54 +0200

    Added a static areas for footer

--- functions.php
+++ functions.php
@@ -49,6 +49,14 @@ function cherry_theme_setup() {
 		'name'           => __( 'Header Bottom', 'cherry' ),
 		'container_wrap' => false,
 	) );
+	cherry_register_static_area( array(
+		'id'             => 'footer-top',
+		'name'           => __( 'Footer Top', 'cherry' ),
+	) );
+	cherry_register_static_area( array(
+		'id'             => 'footer-bottom',
+		'name'           => __( 'Footer Bottom', 'cherry' ),
+	) );
 
 	// Registered a static elements.
 	cherry_register_static( array(
@@ -143,6 +151,20 @@ function cherry_theme_setup() {
 		'options' => array(
 			'priority' => 3,
 			'area'     => 'header-bottom',
+		)
+	) );
+
+	cherry_register_static( array(
+		'id'      => 'footer_menu',
+		'name'    => __( 'Footer Menu', 'cherry4' ),
+		'options' => array(
+			'col-lg'   => 'col-lg-8',
+			'col-md'   => 'col-md-8',
+			'col-sm'   => 'col-sm-12',
+			'col-xs'   => 'col-xs-12',
+			'class'    => 'custom-footer-menu',
+			'priority' => 1,
+			'area'     => 'footer-top',
 		)
 	) );
 
