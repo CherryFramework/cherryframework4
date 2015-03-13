0317c783781e0b41e84bb46eb5f29bdaa0b86589
maksim <maksim.info.555@gmail.com>
2015-03-13 12:37:35 +0200

    ADD: framework sass, dynamic css, dev

--- inc/dynamic-style.php
+++ inc/dynamic-style.php
@@ -16,18 +16,121 @@ if ( !defined( 'WPINC' ) ) {
 
 $cherry_css_vars = cherry_get_css_varaibles();
 
+$body_typography = $cherry_css_vars['typography-body-text'] ;
+
 ?>
+
+body {
+	color: <?php echo cherry_esc_value( $body_typography, 'color' ); ?>;
+}
 
 .cherry-btn-primary {
 	background-color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
 	color: <?php echo cherry_contrast_color( cherry_esc_value( $cherry_css_vars, 'color-primary' ) ); ?>;
+	border: 2px solid <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
 }
-.cherry-btn-primary:hover{
+.cherry-btn-primary:hover {
 	background-color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-secondary' ); ?>;
+	border: 2px solid <?php echo cherry_esc_value( $cherry_css_vars, 'color-secondary' ); ?>;
 }
 .cherry-btn-link{
 	color: <?php echo cherry_contrast_color( cherry_esc_value( $cherry_css_vars, 'color-primary' ) ); ?>;
 }
-.cherry-btn-link:hover{
+.cherry-btn-link:hover {
 	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-secondary' ); ?>;
+}
+.cherry-btn-primary-light {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+	border: 2px solid <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-btn-primary-light:hover {
+	background-color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-btn-gray {
+	border: 2px solid <?php echo cherry_colors_lighten( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 54); ?>;
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-secondary' ); ?>;
+}
+.cherry-btn-gray:hover {
+	background-color: <?php echo cherry_colors_lighten( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 54); ?>;
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-secondary' ); ?>;
+}
+
+.cherry-list-numbered-circle > li, .cherry-list-numbered-slash > li, .cherry-list-icons > li {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+
+.cherry-list-numbered-circle > li::before {
+	background-color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-list-numbered-circle > li:hover::before {
+	background-color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+.cherry-list-numbered-circle > li:hover {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+
+
+.cherry-list-simple > li {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-list-simple > li::before {
+	color: <?php echo cherry_colors_lighten( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 53.2); ?>;
+}
+.cherry-list-simple > li:hover {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+.cherry-list-simple > li:hover::before {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+
+
+.cherry-list-numbered-slash > li::before {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-list-numbered-slash > li:hover {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-list-numbered-slash > li:hover::before {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+
+
+.cherry-list-icons > li {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+.cherry-list-icons > li:hover {
+	color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-list-icons > li i {
+	color: <?php echo cherry_colors_lighten( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 53.2); ?>;
+}
+.cherry-list-icons > li:hover i {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+
+.cherry-hr-primary{
+	background-color: <?php echo cherry_esc_value( $cherry_css_vars, 'color-primary' ); ?>;
+}
+.cherry-hr-gray-lighter{
+	background-color: <?php echo cherry_colors_lighten( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 53.2); ?>;
+}
+.cherry-hr-gray-dark{
+	background-color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+
+
+
+
+
+.help-block {
+	color: <?php echo cherry_colors_lighten( cherry_esc_value( $body_typography, 'color' ), 20); ?>;
+}
+legend {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 20); ?>;
+}
+.cherry-highlight-grey {
+	color: <?php echo cherry_colors_darken( cherry_esc_value( $cherry_css_vars, 'color-gray-variations' ), 13.2); ?>;
+}
+output {
+	<?php echo cherry_esc_value( $cherry_css_vars, 'color-gray-variations' );?>
 }
