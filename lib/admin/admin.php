4c7378011afd324f94e04f61b0035f591a3630e3
cheh <chehovskiy.dima@gmail.com>
2015-05-11 17:47:13 +0300

    ADD: File Header

--- lib/admin/admin.php
+++ lib/admin/admin.php
@@ -1,9 +1,4 @@
 <?php
-// If this file is called directly, abort.
-if ( !defined( 'WPINC' ) ) {
-	die;
-}
-
 /**
  * Theme administration functions used with other components of the framework admin.
  * This file is for setting up any basic features and holding additional admin helper functions.
