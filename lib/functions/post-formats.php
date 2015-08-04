3e83a349983e469446c97f678d28a225f8fe3e92
cheh <chehovskiy.dima@gmail.com>
2015-08-04 15:23:42 +0300

    UPD: `format-link-marker`

--- lib/functions/post-formats.php
+++ lib/functions/post-formats.php
@@ -102,7 +102,9 @@ function cherry_get_the_link_title( $title, $post_id ) {
 		return $title;
 	}
 
-	if ( ! post_type_supports( get_post_type( $post_id ), 'post-formats' ) ) {
+	$post_type = get_post_type( $post_id );
+
+	if ( 'post' !== get_post_type( $post_id ) ) {
 		return $title;
 	}
 
@@ -110,7 +112,7 @@ function cherry_get_the_link_title( $title, $post_id ) {
 		return $title;
 	}
 
-	return $title . '<span class="format-link-marker">&rarr;</span>';
+	return $title . '<span class="format-link-marker"></span>';
 }
 
 /**
